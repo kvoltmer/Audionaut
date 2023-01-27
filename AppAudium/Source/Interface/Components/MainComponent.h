b4e3c05938fabd537c6de6c29d75d4b040d6ecf7
Klaus Voltmer <vltmrkls@gmail.com>
2023-01-27 11:22:52 +0100

    introduce new code

---
+++ AppAudium/Source/Interface/Components/MainComponent.h
@@ -0,0 +1,90 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Projucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 7.0.2
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library.
+  Copyright (c) 2020 - Raw Material Software Limited.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+//[Headers]     -- You can add your own extra header files here --
+#include <JuceHeader.h>
+
+using namespace juce;
+
+class WaveFormComponent;
+
+//[/Headers]
+
+
+
+//==============================================================================
+/**
+                                                                    //[Comments]
+    An auto-generated component, created by the Projucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
+*/
+class MainComponent  : public juce::Component,
+                       private juce::ChangeListener,
+                       public juce::Slider::Listener
+{
+public:
+    //==============================================================================
+    MainComponent ();
+    ~MainComponent() override;
+
+    //==============================================================================
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    void changeListenerCallback (ChangeBroadcaster* source) override;
+    void showAudioResource (URL resource);
+    bool loadURLIntoTransport (const URL& audioURL);
+    //[/UserMethods]
+
+    void paint (juce::Graphics& g) override;
+    void resized() override;
+    void sliderValueChanged (juce::Slider* sliderThatWasMoved) override;
+
+
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    std::unique_ptr<WaveFormComponent> waveFormComponent;
+
+    AudioFormatManager formatManager;
+    TimeSliceThread thread  { "audio file preview" };
+
+    AudioDeviceManager audioDeviceManager;
+
+    URL currentAudioFile;
+    AudioSourcePlayer audioSourcePlayer;
+    AudioTransportSource transportSource;
+    std::unique_ptr<AudioFormatReaderSource> currentAudioFileSource;
+
+    //[/UserVariables]
+
+    //==============================================================================
+    std::unique_ptr<juce::Viewport> waveFormViewport;
+    std::unique_ptr<juce::Slider> zoomSlider;
+
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+};
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
+
