b24f98ec07bba0009ae75839c64e3bac09258133
Klaus Voltmer <vltmrkls@gmail.com>
2023-09-28 11:24:53 +0200

    rename WaveFormTable and friends to AudioGroup

--- AppAudium/Source/Interface/Models/AudioGroupListBoxModel.h
+++ AppAudium/Source/Interface/Models/AudioGroupListBoxModel.h
@@ -1,12 +1,3 @@
-/*
-  ==============================================================================
-
-    WaveFormListBoxModel.h
-    Created: 2 Feb 2023 5:15:07pm
-    Author:  Klaus Voltmer
-
-  ==============================================================================
-*/
 
 #pragma once
 #include <vector>
@@ -17,19 +8,19 @@
 #include "Interface/Components/WaveFormComponent.h"
 #include "Interface/Handlers/ZoomHandler.h"
 #include "Interface/Widgets/audium_ListBox.h"
-#include "Interface/Controls/WaveFormTableListBox.h"
+#include "Interface/Controls/AudioGroupListBox.h"
 #include "Interface/Controls/RegionSelector.h"
 
-class WaveFormTableListBoxModel : public audium::ListBoxModel {
+class AudioGroupListBoxModel : public audium::ListBoxModel {
     
 public:
     
-    WaveFormTableListBoxModel(std::shared_ptr<WaveFormTableListBox> owner,
+    AudioGroupListBoxModel(std::shared_ptr<AudioGroupListBox> owner,
                               std::shared_ptr<AudioResourceContainer> audioResourceContainer,
                               std::shared_ptr<PlayListContainer> playListContainer,
                               std::shared_ptr<ZoomHandler> zoomHandler,
                               std::shared_ptr<RegionSelector> RegionSelector);
-    ~WaveFormTableListBoxModel();
+    ~AudioGroupListBoxModel();
     
     int getNumRows() override;
 
@@ -59,6 +50,6 @@ private:
     /// remove this
     std::shared_ptr<RegionSelector> regionSelector;
     
-    std::shared_ptr<WaveFormTableListBox> owner;
+    std::shared_ptr<AudioGroupListBox> owner;
 
 };
