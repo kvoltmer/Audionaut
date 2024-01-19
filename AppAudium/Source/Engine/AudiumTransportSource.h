22404f8d4eb0298cad4e52580a7f03a1806f86f8
Klaus Voltmer <vltmrkls@gmail.com>
2024-01-19 17:11:42 +0100

    mono channel view (left/right)

--- AppAudium/Source/Engine/AudiumTransportSource.h
+++ AppAudium/Source/Engine/AudiumTransportSource.h
@@ -14,6 +14,8 @@
 #include "Engine/PlayList/SampleTimer.h"
 #include "Engine/Playback/audium_AudioTransportSource.h"
 
+#define MAX_AUDIO_FILE_CHANNELS 64
+
 class AudioGroup;
 
 class AudiumTransportSource : public audium::AudioTransportSource
@@ -90,16 +92,25 @@ public:
             tBase::getNextAudioBlock(infoPart2);
         }
         
-        outputLevel = info.buffer->getMagnitude(0, info.startSample, info.numSamples);
+        for (auto i = 0; i < info.buffer->getNumChannels(); i++)
+        {
+            outputLevel[i] = info.buffer->getMagnitude(i, info.startSample, info.numSamples);
+        }
     }
     
-    float getOutputLevel() const { return outputLevel.load(); }
+    float getOutputLevel(int channel) const
+    {
+        if (channel < MAX_AUDIO_FILE_CHANNELS)
+            return outputLevel[channel].load();
+        
+        return 0.f;
+    }
     
 private:
     
     typedef audium::AudioTransportSource tBase;
     
-    std::atomic<float> outputLevel;
+    std::atomic<float> outputLevel[MAX_AUDIO_FILE_CHANNELS];
     
     // the sample position where the position change should happen
     std::atomic<int> scheduledSample = 0;
