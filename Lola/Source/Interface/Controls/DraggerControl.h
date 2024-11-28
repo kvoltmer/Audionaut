d6bfe3c085a475c2691a1dce290b09f840a3157c
Klaus Voltmer <vltmrkls@gmail.com>
2024-11-28 16:49:25 +0100

    deselect on mouse up

--- Lola/Source/Interface/Controls/DraggerControl.h
+++ Lola/Source/Interface/Controls/DraggerControl.h
@@ -87,87 +87,10 @@ public:
         outsideEdge
     };
     
-    void mouseDown (const juce::MouseEvent& e) override
-    {
-        if (regionSelector != nullptr)
-            regionSelector->setEnabled(false);
-
-        currentDragMode = getDragMode(e.getPosition().getX());
-        
-        originalBounds = componentToDrag->getBounds();
-        
-        setSelected(e.mods.isCommandDown() ? !isSelected() : true, !e.mods.isCommandDown());
-        
-        auto rangeInClocks = zoomHandler->xToClocks(componentToDrag->getBounds().toDouble().getHorizontalRange());
-        zoomHandler->getSnapToGridHandler()->publishRange(rangeInClocks);
-        
-        mouseDownOffset = getLocalPoint (this, e.position);
-        
-        repaint();
-    }
-
-    void mouseDrag (const juce::MouseEvent& e) override
-    {
-        if (e.mods.isAltDown())
-        {
-            if (juce::DragAndDropContainer* container = juce::DragAndDropContainer::findParentDragContainerFor(componentToDrag))
-            {
-                container->startDragging("DraggerControl", componentToDrag);
-            }
-        }
-        else
-        {
-            auto distance = e.getOffsetFromDragStart();
-            distance.setY(0); // drag vertically only
-            if (std::abs(distance.getX()) > 0)
-            {
-                auto bounds = originalBounds;
-                
-                switch (currentDragMode) {
-                    case leftEdge:
-                        bounds.setLeft(juce::jmin(originalBounds.getRight() - minimumWidth, originalBounds.getX() + distance.getX()));
-                        break;
-                    case rightEdge:
-                        bounds.setRight(juce::jmax(originalBounds.getX() + minimumWidth, originalBounds.getRight() + distance.getX()));
-                        break;
-                    case middleEdge:
-                        bounds += distance;
-                        break;
-                    default:
-                        jassertfalse;
-                        break;
-                }
-                
-                componentToDrag->setBounds (bounds);
-                
-                auto rangeInClocks = zoomHandler->xToClocks(componentToDrag->getBounds().toDouble().getHorizontalRange());
-                zoomHandler->getSnapToGridHandler()->publishRange(rangeInClocks);
-                commitRangeToEngine(rangeInClocks);
-            }
-        }
-    }
-
-    void mouseUp (const juce::MouseEvent& e) override
-    {
-        if (regionSelector != nullptr)
-            regionSelector->setEnabled(true);
-        
-        if (std::abs(e.getOffsetFromDragStart().getX()) > 0)
-        {
-            auto rangeInClocks = zoomHandler->xToClocks(componentToDrag->getBounds().toDouble().getHorizontalRange());
-            zoomHandler->snapToGrid(rangeInClocks);
-            commitRangeToEngine(rangeInClocks);
-            validateData();
-            sendChangeMessage();
-        }
-        
-        zoomHandler->getSnapToGridHandler()->clearRange();
-    }
-
-    void mouseMove (const juce::MouseEvent& e) override
-    {
-        updateMouseZone (e);
-    }
+    void mouseDown (const juce::MouseEvent& e) override;
+    void mouseUp (const juce::MouseEvent& e) override;
+    void mouseDrag (const juce::MouseEvent& e) override;
+    void mouseMove (const juce::MouseEvent& e) override;
 
     void updateMouseZone (const juce::MouseEvent& e)
     {
