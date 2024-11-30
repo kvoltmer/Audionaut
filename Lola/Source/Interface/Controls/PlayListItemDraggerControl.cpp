5214cc3d35c27fb9a149e5a190d4b02840ece6a9
Klaus Voltmer <vltmrkls@gmail.com>
2024-11-30 11:05:29 +0100

    cleanup dragger control

--- Lola/Source/Interface/Controls/PlayListItemDraggerControl.cpp
+++ Lola/Source/Interface/Controls/PlayListItemDraggerControl.cpp
@@ -10,18 +10,6 @@
 
 #include "PlayListItemDraggerControl.h"
 
-void PlayListItemDraggerControl::commitData(const juce::Range<double> newData, audium::TimeContextType context)
-{
-    // undo
-    // TODO: move to base class
-    if (undoableContainerAction == nullptr)
-    {
-        undoableContainerAction = std::make_unique<audium::UndoableContainerAction>(*audiumEngine->getAudioTrackContainer(), false);
-    }
-    
-    commitPositionData(*playListItem.get(), newData, context);
-}
-
 bool PlayListItemDraggerControl::validateData()
 {
     bool result = playListItem->validateData();
@@ -29,14 +17,6 @@ bool PlayListItemDraggerControl::validateData()
     // sort by position
     playListContainer->sortByPosition();
     
-    // TODO: move to base class
-    if (undoableContainerAction != nullptr)
-    {
-        // Undo: store new state
-        undoableContainerAction->storeNewState();
-        audiumEngine->getUndoManager()->perform(undoableContainerAction.release(), "Modify Item");
-        audiumEngine->getUndoManager()->beginNewTransaction();
-    }
     
     return result;
 }
