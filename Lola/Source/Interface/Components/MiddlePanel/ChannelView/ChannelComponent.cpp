5244f813474ad5d98cbb0049e3610ad69d4f335d
Klaus Voltmer <vltmrkls@gmail.com>
2025-01-20 18:43:42 +0100

    bugfix channel meter (track offset)

--- Lola/Source/Interface/Components/MiddlePanel/ChannelView/ChannelComponent.cpp
+++ Lola/Source/Interface/Components/MiddlePanel/ChannelView/ChannelComponent.cpp
@@ -134,8 +134,6 @@ void ChannelComponent::refreshComponent(std::shared_ptr<AudioTrack> audioTrack_,
 {
     audioTrack = audioTrack_;
     rowNumber = rowNumber_;
-
-    channelNumber = audioTrack->getChannel(rowNumber)->getChannelNumber();
     
     volumeSlider->setValue(LevelMeter::gainToDecebel(audioTrack->getGain(rowNumber)), dontSendNotification);
     panSlider->setValue(audioTrack->getPan(rowNumber), dontSendNotification);
@@ -143,6 +141,8 @@ void ChannelComponent::refreshComponent(std::shared_ptr<AudioTrack> audioTrack_,
     if (not isTimerRunning()) {
         startTimerHz(60);
     }
+    
+    channelNumber = audioTrack->getChannel(rowNumber)->getChannelNumber() + audioTrack->getChannelOffset();
 }
 
 void ChannelComponent::timerCallback()
