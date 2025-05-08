e8eba2c3d7c33b2ef22a07b58a2d72f1b7e84fb7
Klaus Voltmer <vltmrkls@gmail.com>
2025-05-08 14:48:54 +0200

    fix: region list multiple select -> drop -> wrong order

--- Audionaut/Source/Engine/Group/AudioTrack.h
+++ Audionaut/Source/Engine/Group/AudioTrack.h
@@ -233,6 +233,7 @@ public:
     
     std::shared_ptr<AudioRegion> getRegion(int rowNumber) const;
     const std::vector<std::shared_ptr<AudioRegion>> getRegions() const;
+    int getAudioRegionId(std::shared_ptr<const AudioRegion> searchRegion) const;
     int lastRegionSelected = -1; ///< Index of the last region selected.
     
 private:
