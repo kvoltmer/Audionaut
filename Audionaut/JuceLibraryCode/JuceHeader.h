0480a482c32d68f510817eef4da2b5fa1e47e591
Klaus Voltmer <vltmrkls@gmail.com>
2025-05-08 15:08:06 +0200

    version 1.0.1

--- Audionaut/JuceLibraryCode/JuceHeader.h
+++ Audionaut/JuceLibraryCode/JuceHeader.h
@@ -44,7 +44,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Audionaut";
     const char* const  companyName    = "Klaus Voltmer";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  versionString  = "1.0.1";
+    const int          versionNumber  = 0x10001;
 }
 #endif
